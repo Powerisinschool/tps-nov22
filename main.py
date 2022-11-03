9976f046554c1c49a83be5784d55c25b0525a49e
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-11-03 11:30:29 +0100

    /home/tolu/.local/bin/gitter

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import glob
 import os
 import gc
 
+from tqdm import tqdm, trange
+
 import warnings
 warnings.simplefilter('ignore')
 
@@ -24,4 +26,31 @@ def seed_everything(seed=42):
 seed_everything(CFG.seed)
 
 labels = pd.read_csv(os.path.join(CFG.path, "train_labels.csv"))
-submission = pd.read_csv(os.path.join(CFG.path, "sample_submission.csv"))
+#submission = pd.read_csv(os.path.join(CFG.path, "sample_submission.csv"))
+
+labels = labels.astype({"label": "int8"}).dtypes
+
+all_df = pd.DataFrame(np.zeros(40000), columns=['id'])
+all_df['id'] = all_df.index
+
+file_list = sorted(glob.glob(os.path.join(CFG.path, "submission_files/*.csv")))
+for idx, file in enumerate(tqdm(file_list)):
+    all_df[f'pred_{idx}'] = pd.read_csv(file)[CFG.pred]
+
+train = all_df[:labels.shape[0]]
+train[CFG.target] = labels[CFG.target]
+test = all_df[labels.shape[0]:].reset_index(drop=True)
+
+del labels
+del all_df
+gc.collect()
+
+train.to_csv('train.csv')
+
+del train
+gc.collect()
+
+test.to_csv('test.csv')
+
+del test
+gc.collect()
